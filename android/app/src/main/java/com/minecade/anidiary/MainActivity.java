032ef1002ebc28398f88105ecda59588424f0075
Minecade <anitabugade42@gmail.com>
2026-09-12 17:27:27 +0530

    Change onResume method to public visibility

--- android/app/src/main/java/com/minecade/anidiary/MainActivity.java
+++ android/app/src/main/java/com/minecade/anidiary/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends BridgeActivity {
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         enableImmersiveMode();
     }
